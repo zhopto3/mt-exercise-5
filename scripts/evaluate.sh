1ee803803a10b50cae4f6faed5f9927e1b6d181c
moritz-steiner <128054784+moritz-steiner@users.noreply.github.com>
2023-05-15 16:58:14 +0000

    Add files via upload

---
+++ scripts/evaluate.sh
@@ -0,0 +1,46 @@
+#! /bin/bash
+
+scripts=$(dirname "$0")
+base=$scripts/..
+
+data=$base/sampled_data
+configs=$base/configs
+
+translations=$base/translations
+
+mkdir -p $translations
+
+src=?
+trg=?
+
+
+num_threads=4
+device=0
+
+# measure time
+
+SECONDS=0
+
+model_name=?
+
+echo "###############################################################################"
+echo "model_name $model_name"
+
+translations_sub=$translations/$model_name
+
+mkdir -p $translations_sub
+
+CUDA_VISIBLE_DEVICES=$device OMP_NUM_THREADS=$num_threads python -m joeynmt translate $configs/$model_name.yaml < $data/test.$src > $translations_sub/test.$model_name.$trg
+
+# remove first line with "sentence" to make hyp and ref equal length
+
+tail -n +2 $translations_sub/test.$model_name.$trg > $translations_sub/test.$model_name.clean.$trg
+mv $translations_sub/test.$model_name.clean.$trg $translations_sub/test.$model_name.$trg
+
+# compute case-sensitive BLEU 
+
+cat $translations_sub/test.$model_name.$trg | sacrebleu $data/test.$trg
+
+
+echo "time taken:"
+echo "$SECONDS seconds"
